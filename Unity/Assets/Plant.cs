3618b542d12ffe6dccb95ef4809de407c0e61cf3
Ben Hardy <patben8@gmail.com>
2018-11-19 12:11:23 -0300

    Inicio del juego.

---
+++ Unity/Assets/Plant.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Plant : MonoBehaviour {
+
+    // Use this for initialization
+    public SpriteRenderer sprite;
+    public Sprite[] plantSprites;
+    public float plantGrowth;
+
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        sprite.sprite = plantSprites[Mathf.Clamp(Mathf.FloorToInt(plantGrowth / plantSprites.Length),0,plantSprites.Length-1)];
+	}
+}
